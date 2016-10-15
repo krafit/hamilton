fcc7fc14570505f644c603316e7c4328639b8664
Simon Kraft <simon@krafit.de>
2016-10-15 21:50:25 +0200

    Edit-Tag entfernt

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -67,15 +67,6 @@ function hamilton_entry_footer() {
 		echo '</span>';
 	}
 
-	edit_post_link(
-		sprintf(
-			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'hamilton' ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		),
-		'<span class="edit-link">',
-		'</span>'
-	);
 }
 endif;
 
